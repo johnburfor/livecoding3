01d3dc5f5b3f3800c37679394a77f80897c0008a
Johnny Burford <johnnyburford75@gmail.com>
2019-12-03 19:09:40 -0500

    finished livecoding3

---
+++ Zip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        array_one = [1, 5, 2];
+        array_two = [3, 6, 4];
+
+        foreach i in array_one{
+            foreach j in array_two{
+                zipped_array = array_one[i].append, array_two[j].append}
+        }
+
+	}
+}
